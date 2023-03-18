071128a6806912e5a2803b512d59e11b99ce028d
Dhanush Naik <dhanushlnaik@gmail.com>
2023-03-18 07:02:14 +0530

    📜️README

--- app/src/main/java/com/example/vwent/ChatInterface.kt
+++ app/src/main/java/com/example/vwent/ChatInterface.kt
@@ -1,24 +1,42 @@
 package com.example.vwent
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 import android.widget.Button
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.pusher.client.Pusher
+import com.pusher.client.PusherOptions
+import com.pusher.client.connection.ConnectionEventListener
+import com.pusher.client.connection.ConnectionState
+import com.pusher.client.connection.ConnectionStateChange
 
 class ChatInterface : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat_interface)
-        clickListener()
+            val options = PusherOptions()
+            options.setCluster("ap2");
 
-    }
-    public fun clickListener(){
-        var but = findViewById<Button>(R.id.button3)
+            val pusher = Pusher("2164fb3abb66b1351f88", options)
 
-        but.setOnClickListener {
-            startActivity(Intent(this@ChatInterface, satisfied::class.java))
+            pusher.connect(object : ConnectionEventListener {
+                override fun onConnectionStateChange(change: ConnectionStateChange) {
+                    Log.i("Pusher", "State changed from ${change.previousState} to ${change.currentState}")
+                }
+
+                override fun onError(
+                    message: String,
+                    code: String,
+                    e: Exception
+                ) {
+                    Log.i("Pusher", "There was a problem connecting! code ($code), message ($message), exception($e)")
+                }
+            }, ConnectionState.ALL)
+
+            val channel = pusher.subscribe("my-channel")
+            channel.bind("my-event") { event ->
+                Log.i("Pusher","Received event with data: $event")
+            }
         }
-
-
     }
-}
---
+++ app/src/main/java/com/example/vwent/PaymentGateway.kt
@@ -0,0 +1,11 @@
+package com.example.vwent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PaymentGateway : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_payment_gateway)
+    }
+}
